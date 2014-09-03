2454c0cbe22b232f332ad3d76c4b0d0665cc8280
linfengchina <linfeng@linfeng-iOS.local>
2014-09-03 09:04:44 +0800

    change author name
    change author name

--- ForbidEmojiDemo/main.m
+++ ForbidEmojiDemo/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  ForbidEmojiDemo
 //
-//  Created by 林峰 on 14-9-2.
+//  Created by pigpigdaddy on 14-9-2.
 //  Copyright (c) 2014年 pigpigdaddy. All rights reserved.
 //
 
--- ForbidEmojiDemoTests/ForbidEmojiDemoTests.m
+++ ForbidEmojiDemoTests/ForbidEmojiDemoTests.m
@@ -2,7 +2,7 @@
 //  ForbidEmojiDemoTests.m
 //  ForbidEmojiDemoTests
 //
-//  Created by 林峰 on 14-9-2.
+//  Created by pigpigdaddy on 14-9-2.
 //  Copyright (c) 2014年 pigpigdaddy. All rights reserved.
 //
 
